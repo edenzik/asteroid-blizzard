53db12a9d8b80000d5d5a2e871fe25d38419f6ec
Kahlil Oppenheimer <kahliloppenheimer@gmail.com>
2015-11-23 22:16:35 -0500

    Modified camera slightly

--- main/camera.js
+++ main/camera.js
@@ -1,11 +1,11 @@
-var camera = new THREE.TargetCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+var camera = new THREE.TargetCamera(45, window.innerWidth / window.innerHeight, 0.1, 2000);
 
 camera.addTarget( {
     name: 'avatar',
     targetObject: avatar,
     cameraPosition: new THREE.Vector3(0, 0, -15),
     fixed: false,
-    stiffness: .1,
+    stiffness: .5,
     matchRotation: false
 });
 
--- main/starforge.js
+++ main/starforge.js
@@ -1,13 +1,17 @@
 // Last position at which stars were generated
 var lastPos = null;
 function renderStars() {
-    if (lastPos != null && distance(lastPos, avatar.position) < (STAR_RENDER_DISTANCE / 3)) {
+    if (lastPos != null) {
+        console.log(distance(lastPos, avatar.position));
+    }
+    if (lastPos != null && distance(lastPos, avatar.position) < 5) {
+        console.log('skipped!');
         return;
     }
     lastPos = new THREE.Vector3().copy(avatar.position);
     // Number of stars is cubically related to the render distance
     // because as distance increases linearly, volume increases cubically
-    var starQty = Math.random() * (.05 * Math.pow(STAR_RENDER_DISTANCE, 3));
+    var starQty = Math.random() * (.2 * Math.pow(STAR_RENDER_DISTANCE, 3));
     starGeo = new THREE.SphereGeometry(50);
 
     materialOptions = {
@@ -19,19 +23,20 @@ function renderStars() {
 
     for (var i = 0; i < starQty; i++) {
         var starVertex = new THREE.Vector3();
-        starVertex.x = camera.position.x + Math.random() * STAR_RENDER_DISTANCE - (STAR_RENDER_DISTANCE / 2);
-        starVertex.y = camera.position.y + Math.random() * STAR_RENDER_DISTANCE - (STAR_RENDER_DISTANCE / 2);
-        starVertex.z = camera.position.z + Math.random() * STAR_RENDER_DISTANCE - (STAR_RENDER_DISTANCE / 2);
+        starVertex.x = camera.position.x + Math.random() * 20 + -10;
+        starVertex.y = camera.position.x + Math.random() * 20 + -10;
+        starVertex.z = camera.position.x + Math.random() * 20 + -10;
+
 
         starGeo.vertices.push(starVertex);
     }
 
-    stars = new THREE.Points(starGeo, starStuff);
+    var stars = new THREE.Points(starGeo, starStuff);
     stars.position.x = avatar.position.x;
     stars.position.y = avatar.position.y;
     stars.position.z = avatar.position.z;
     stars.name = "stars";
-    
+
     scene.add(stars);
 }
 
@@ -41,4 +46,9 @@ function distance(v, w) {
     var dy = w.y - v.y;
     var dz = w.z - v.z;
     return Math.sqrt(dx * dx + dy * dy + dz * dz);
+}
+
+// Returns a number psuedorandomly distributed between min and max
+function randRange(min, max) {
+    return min + Math.random() * ((max - min) + 1);
 }
