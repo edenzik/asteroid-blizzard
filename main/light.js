c99cbc2b6b155ebf7274e1d8f50dba9e1654ea5e
Kahlil Oppenheimer <kahliloppenheimer@gmail.com>
2015-12-01 21:18:44 -0500

    Added comment to lights

--- main/light.js
+++ main/light.js
@@ -2,33 +2,10 @@
 var ambientLight = new THREE.AmbientLight(0xffffff / 8);
 scene.add(ambientLight);
 
+// Directional light to simulate sunlight
 var directional = new THREE.DirectionalLight(0xffffff, 1.0);
 directional.position.set(1, 0, 0);
 scene.add(directional);
-
-//create two spotlights to illuminate the scene
-/**
-var spotLight = new THREE.SpotLight( 0xffffff );
-spotLight.position.set( -40, 60, -10 );
-spotLight.intensity = 2;
-console.log(spotLight);
-spotLight.matrixAutoUpdate = true;
-scene.add( spotLight );
-
-var spotLight2 = new THREE.SpotLight( 0x5192e9 );
-spotLight2.position.set( 40, -60, 30 );
-spotLight2.intensity = 1.5;
-spotLight2.matrixAutoUpdate = true;
-scene.add( spotLight2 );
-
-// add spotlight for the shadows
-var spotLight3 = new THREE.SpotLight(0xffffff);
-spotLight3.position.set(-40, 40, 50);
-spotLight3.castShadow = true;
-spotLight3.matrixAutoUpdate = true;
-scene.add(spotLight3);
-**/
-
 
 // Adjust lights so they follow the spaceship
 function updateLights() {
