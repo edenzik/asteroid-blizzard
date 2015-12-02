43d279f45abb18d2854ad340eb000504ff66ff2b
edenzik <edenzik@gmail.com>
2015-12-02 15:24:50 -0500

    weird not working?

--- bower_components/threex.planets/threex.planets.js
+++ bower_components/threex.planets/threex.planets.js
@@ -43,10 +43,10 @@ THREEx.Planets.createVenus	= function(){
 THREEx.Planets.createEarth	= function(){
 	var geometry	= new THREE.SphereGeometry(0.5, 32, 32)
 	var material	= new THREE.MeshPhongMaterial({
-		map		: THREE.ImageUtils.loadTexture('../images/earthmap1k.jpg'),
-		bumpMap		: THREE.ImageUtils.loadTexture('../images/earthbump1k.jpg'),
+		map		: THREE.ImageUtils.loadTexture('./images/earthmap1k.jpg'),
+		bumpMap		: THREE.ImageUtils.loadTexture('./images/earthbump1k.jpg'),
 		bumpScale	: 0.05,
-		specularMap	: THREE.ImageUtils.loadTexture('../images/earthspec1k.jpg'),
+		specularMap	: THREE.ImageUtils.loadTexture('./images/earthspec1k.jpg'),
 		specular	: new THREE.Color('grey'),
 	})
 	var mesh	= new THREE.Mesh(geometry, material)
